a6e32cca7d71872383c865b564dfec2ad27bdbd1
mandalale <adikarasencoko@gmail.com>
2024-11-19 10:55:49 +0700

    Add files via upload

---
+++ quiz21.py
@@ -0,0 +1,16 @@
+var = len([1,2,3,4])
+print (var)
+
+# append
+data = [1,2]
+data.append(3)
+print(data)
+
+k=7//3
+print (k)
+
+b="applepie".replace ("pie","cake")
+print(b)
+
+for i in[1,2,3,]:
+    print
